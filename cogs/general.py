831b40d996f857423eaa394be831326be28807ab
poke5352-ok <poke5352@gmail.com>
2020-07-18 09:36:17 -0700

    feat: add member count

--- cogs/general.py
+++ cogs/general.py
@@ -10,6 +10,24 @@ class general(commands.Cog):
     async def ping(self, ctx):
         await ctx.send(f'Pong! {round(self.bot.latency * 1000) }ms') 
 
+    async def presence_update(self):
+        guild = self.bot.get_guild(704488514112192573)
+        if guild:
+            await self.bot.change_presence(activity=discord.Game(name=f"with {guild.member_count} users"))
+            return
+        
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.presence_update()
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        await self.presence_update()
+
+    @commands.Cog.listener()
+    async def on_member_leave(self, member):
+        await self.presence_update()
 
 
 def setup(bot):
